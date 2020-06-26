87d88e46f8bc12a5fecc3cca42d7c721d0500151
gameprog12 <64625480+gameprog12@users.noreply.github.com>
2020-06-26 20:20:11 +0900

    feat(create): モンスター選択カルーセル追加

--- src/app/create/create.module.ts
+++ src/app/create/create.module.ts
@@ -8,7 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRadioModule } from '@angular/material/radio';
-
+import { SwiperModule } from 'ngx-swiper-wrapper';
 @NgModule({
   declarations: [CreateComponent],
   imports: [
@@ -20,6 +20,7 @@ import { MatRadioModule } from '@angular/material/radio';
     MatFormFieldModule,
     MatButtonModule,
     MatRadioModule,
+    SwiperModule,
   ],
 })
 export class CreateModule {}
