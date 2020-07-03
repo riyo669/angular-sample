7eebe6dcfcca0149c8bc855730e82c4819e7891a
gameprog12 <64625480+gameprog12@users.noreply.github.com>
2020-07-03 19:28:54 +0900

    feat(auth): ログイン処理の実装

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth, User } from 'firebase/app';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  // 最新のログイン情報をとれるストリーム
+  afUser$: Observable<User> = this.afAuth.user;
 
-  login() {}
+  constructor(private afAuth: AngularFireAuth) {
+    this.afUser$.subscribe((user) => console.log(user));
+  }
+
+  login() {
+    // ログインするとポップアップでGithubログイン画面が開く
+    this.afAuth.signInWithPopup(new auth.GithubAuthProvider());
+  }
 
   logout() {}
 }
