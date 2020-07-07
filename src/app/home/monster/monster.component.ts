1c7fa1a74fbef06d3ac440d89480a2c862a114cb
gameprog12 <64625480+gameprog12@users.noreply.github.com>
2020-07-07 15:57:48 +0900

    feat(monster): 経験値バーの割合を正しく表示させるようにした

--- src/app/home/monster/monster.component.ts
+++ src/app/home/monster/monster.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Monster } from 'src/app/interfaces/monster';
 
+// 各レベルの累計値
+const expTable = [20, 40, 100, 200, 350, 500, 700, 900, 1100, 1500];
 @Component({
   selector: 'app-monster',
   templateUrl: './monster.component.html',
@@ -8,13 +10,17 @@ import { Monster } from 'src/app/interfaces/monster';
 })
 export class MonsterComponent implements OnInit {
   @Input() monster: Monster;
-  maxExp = 400;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   getExpPercentage(): number {
-    return (this.monster.exp / this.maxExp) * 100;
+    const totalExp = this.monster.exp;
+    const level = this.monster.level;
+    const baseExp = expTable[level - 2] || 0; // 2レベルだとしたら20
+    const nextExp = expTable[level - 1] - baseExp; // 2レベルだとしたら40-20=20
+    const exp = totalExp - baseExp; // 今経験値が30だとしたら30-20=10で3レベルになるのに40必要で今10あるということ
+    return (exp / nextExp) * 100;
   }
 }
