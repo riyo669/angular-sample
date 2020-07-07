84b487b9f4ea35aa2b480553c8cd2c0115d9b1b4
gameprog12 <64625480+gameprog12@users.noreply.github.com>
2020-07-07 14:01:47 +0900

    feat(git-hook): Firebaseのfunction側でレベルを計算させるようにした

--- functions/src/git-hook.function.ts
+++ functions/src/git-hook.function.ts
@@ -5,6 +5,9 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+const expTable = [20, 40, 100, 200, 350, 500, 700, 900, 1100, 1500];
+const GET_EXP = 10;
+
 export const gitHook = functions
   .region('asia-northeast1')
   .https.onRequest(async (request, response) => {
@@ -13,10 +16,22 @@ export const gitHook = functions
       .where('ownerGithubId', '==', request.body.sender.id)
       .get();
 
-    const increment = admin.firestore.FieldValue.increment(10);
+    const monster = monsters.docs[0].data();
+
+    let level = 1;
+    expTable.some((nextExp) => {
+      if (monster.exp + GET_EXP >= nextExp) {
+        level++;
+        return false;
+      } else {
+        return true;
+      }
+    });
+    const increment = admin.firestore.FieldValue.increment(GET_EXP);
     monsters.docs.forEach((doc) =>
       doc.ref.update({
         exp: increment,
+        level,
       })
     );
     response.send('success');
